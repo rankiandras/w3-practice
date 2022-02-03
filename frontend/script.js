8be0e21e6574675b6faaa7e02ec5099e6b6f4700
rankiandras <rankiandras@gmail.com>
2022-02-03 20:38:00 +0100

    header nav added

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,54 @@
-// function addTwoIntegers(firstInteger , secondInteger) {
-//    // console.log(firstInteger + secondInteger)
-//     // return firstInteger + secondInteger
-//     // return `hello`
-//     if(typeof firstInteger === `number` && typeof secondInteger === `number`){
-//         return firstInteger + secondInteger
-//     }else{
-//         return `valamelyik argumentum nem szám, ezért a művelet nem elvégezhető`
-//     }
+// // function addTwoIntegers(firstInteger , secondInteger) {
+// //    // console.log(firstInteger + secondInteger)
+// //     // return firstInteger + secondInteger
+// //     // return `hello`
+// //     if(typeof firstInteger === `number` && typeof secondInteger === `number`){
+// //         return firstInteger + secondInteger
+// //     }else{
+// //         return `valamelyik argumentum nem szám, ezért a művelet nem elvégezhető`
+// //     }
+// // }
+
+
+// // console.log(addTwoIntegers(12 , `13`) );
+// // console.log(addTwoIntegers(1920 , 85) );
+// // console.log( 0 === `0` )
+
+
+// function addTwoStrings(firstString , secondString) {
+//     console.log(firstString + secondString);
+//     return `hello`
 // }
 
 
-// console.log(addTwoIntegers(12 , `13`) );
-// console.log(addTwoIntegers(1920 , 85) );
-// console.log( 0 === `0` )
+// addTwoStrings(`Lili` , `Lali`)
+// addTwoStrings(`Siri` , `Bori`)
 
 
-function addTwoStrings(firstString , secondString) {
-    console.log(firstString + secondString);
-    return `hello`
+function loadEvent(eventObject){
+    // console.log(eventObject);
+    const rootElement = document.getElementById("root");
+    // console.dir(document)
+    console.dir(rootElement);
+
+    const listOfSectionElements = document.querySelectorAll("section");
+    console.dir(listOfSectionElements)
+
+    // rootElement.classList.add("newClass");
+    // listOfSectionElements[0].classList.add("newClass");
+    // listOfSectionElements[1].classList.add("newClass");
+    // listOfSectionElements[2].classList.add("newClass");
+    // listOfSectionElements[3].classList.add("newClass");
+    
+    
+    let anchors = ""
+    
+    for (const sectionElement of listOfSectionElements) {
+    //     sectionElement.classList.add("newClass");
+            // anchors = anchors + `<a> ${ sectionElement.id } </a>`        
+            anchors += `<a href="#${sectionElement.id}"> ${ sectionElement.id } </a>`;
+    }
+    console.log(anchors);
+    rootElement.insertAdjacentHTML("afterbegin", `<header>${ anchors }</header>`)
 }
-
-
-addTwoStrings(`Lili` , `Lali`)
-addTwoStrings(`Siri` , `Bori`)
+window.addEventListener("load", loadEvent);
