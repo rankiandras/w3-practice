0447bed2741b325dec1d4196e8db49af0fe332f4
rankiandras <rankiandras@gmail.com>
2022-02-03 16:02:12 +0100

    first functions

---
+++ frontend/script.js
@@ -0,0 +1,25 @@
+// function addTwoIntegers(firstInteger , secondInteger) {
+//    // console.log(firstInteger + secondInteger)
+//     // return firstInteger + secondInteger
+//     // return `hello`
+//     if(typeof firstInteger === `number` && typeof secondInteger === `number`){
+//         return firstInteger + secondInteger
+//     }else{
+//         return `valamelyik argumentum nem szám, ezért a művelet nem elvégezhető`
+//     }
+// }
+
+
+// console.log(addTwoIntegers(12 , `13`) );
+// console.log(addTwoIntegers(1920 , 85) );
+// console.log( 0 === `0` )
+
+
+function addTwoStrings(firstString , secondString) {
+    console.log(firstString + secondString);
+    return `hello`
+}
+
+
+addTwoStrings(`Lili` , `Lali`)
+addTwoStrings(`Siri` , `Bori`)
